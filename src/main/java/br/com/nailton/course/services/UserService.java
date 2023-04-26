9b293c585708e652f05f2d3a3f420d46bae3f17b
Nailton Sousa <nailton.osn@gmail.com>
2023-04-26 00:12:45 -0300

    Service layer, component registration

---
+++ src/main/java/br/com/nailton/course/services/UserService.java
@@ -0,0 +1,24 @@
+package br.com.nailton.course.services;
+
+import br.com.nailton.course.entities.User;
+import br.com.nailton.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
