ba4ad7e5424a56ef95a868cc3c24060f9f92e999
Nailton Sousa <nailton.osn@gmail.com>
2023-05-01 22:37:10 -0300

    User update

--- src/main/java/br/com/nailton/course/services/UserService.java
+++ src/main/java/br/com/nailton/course/services/UserService.java
@@ -2,6 +2,7 @@ package br.com.nailton.course.services;
 
 import br.com.nailton.course.entities.User;
 import br.com.nailton.course.repositories.UserRepository;
+import org.apache.juli.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,5 +29,17 @@ public class UserService {
 
     public void delete(Long id) {
         repository.deleteById(id);
+    }
+
+    public User update(Long id, User obj) {
+        User entity = repository.getReferenceById(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName((obj.getName()));
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
